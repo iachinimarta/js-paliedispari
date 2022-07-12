94535429bb86ed672719ce3b20d4207c539b4c90
Marta Iachini <iachinimarta@gmail.com>
2022-07-12 23:48:10 +0200

    Risolto esercizio "pari o dispari" senza funzione

--- Pari e Dispari/js/main.js
+++ Pari e Dispari/js/main.js
@@ -2,32 +2,35 @@ const pari = "pari";
 const dispari = "dispari";
 
 // Chiedo all utente di scegliere
-const userChoice = prompt("Scegli tra PARI o DISPARI")
+const userChoice = prompt("Scegli tra PARI o DISPARI");
 console.log(userChoice);
 
-// Chiedo all utente di inserire un numero
-const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
-console.log(userNumber);
+if (userChoice == "pari" | userChoice == "dispari") {
+    const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+    console.log(userNumber);
 
-// Imposto una condizione per l'utente
-if (userNumber > 5 | userNumber < 1) {
-    alert("Valore inserito non corretto!");
-}
+    // Imposto una condizione per l'utente
+    if (userNumber > 5 | userNumber < 1) {
+        alert("Valore inserito non corretto!");
+    }
 
-// Genero un numero casuale per il computer
-const computerNumber = Math.floor(Math.random() * 5) + 1;
-console.log(computerNumber);
+    // Genero un numero casuale per il computer
+    const computerNumber = Math.floor(Math.random() * 5) + 1;
+    console.log(computerNumber);
 
-// Imposto la variabile che calcola la somma dei numeri scelti
-let sommaNumeri = userNumber + computerNumber;
-console.log(sommaNumeri);
+    // Imposto la variabile che calcola la somma dei numeri scelti
+    let sommaNumeri = userNumber + computerNumber;
+    console.log(sommaNumeri);
 
-// Se il risultato della somma è pari e l'utente ha scelto pari
-if (sommaNumeri % 2 == 0 && userChoice == pari) {
-    alert("Hai vinto!");
-// Se il risultato della somma è dispari e l'utente ha scelto dispari
-} else if (sommaNumeri % 2 == 1 && userChoice == dispari) { 
-    alert("Hai vinto!");
+    // Se il risultato della somma è pari e l'utente ha scelto pari
+    if (sommaNumeri % 2 == 0 && userChoice == pari) {
+        alert("Hai vinto!");
+    // Se il risultato della somma è dispari e l'utente ha scelto dispari
+    } else if (sommaNumeri % 2 == 1 && userChoice == dispari) { 
+        alert("Hai vinto!");
+    } else {
+        alert("Ritenta!");
+    }
 } else {
-    alert("Ritenta!");
+    alert("Valore inserito non corretto!");
 }
