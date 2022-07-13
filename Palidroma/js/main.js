ed5a5ccaeaac0cccfb1539ab205c225b82d99195
Marta Iachini <iachinimarta@gmail.com>
2022-07-13 10:08:01 +0200

    Inserita soluzione proposta da Simone

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -2,44 +2,31 @@
 let newWord = prompt("Inserisci una parola");
 console.log(newWord);
 
-// Mia soluzione iniziale con "reverse":
-// // 2.Imposto una funzione in cui al posto di "word" andrà poi la parola inserita dall'utente
-// function checkPalindrome(word) {
-//     // 2.1 Imposto una variabile che mi divide la parola inserita riportando le singole lettere
-//     let newWordSplit = word.split("");
+//Creo una funzione che mi giri la parola
+function giraParola(parolaUtente) {
 
-//     // 2.2 Imposto una variabile che mi inverte l'ordine delle lettere della parola inserita
-//     let newWordReverse = newWordSplit.reverse();
-
-//     // 2.3 Imposto una variabile che mi riunisce le lettere della parola invertita in una nuova parola
-//     let finalNewWord = newWordReverse.join("");
-
-//     // 2.4 Il risultato della funzione sarà la nuova parola creata con la variabile "finalNewWord"
-//     return finalNewWord;
-// }
-
-// // Richiamo la funzione sulla parola inserita dall utente che mi darà come risultato la parola invertita
-// let test = checkPalindrome(newWord);
-// console.log(test);
-
-// // Se la parola inserita dall'utente corrisponde alla parola invertita dopo la funzione ci sarà un alert
-// if (newWord == test) {
-//     alert("Questo è un palindromo!");
-// } else {
-//     alert ("Ritenta!");
-// }
+    /*Soluzione breve con "reverse"
+    const parolagirata = parolaUtente.split('').reverse().join('');*/
 
 
-// Altra soluzione trovata senza "reverse" ma non capita
-let newWordSplit = newWord.split("");
-console.log(newWordSplit);
 
-let wordReverse = [];
+    //Soluzione didattica
+    let parolaGirata = "";
 
-for (let i = newWordSplit.length -1; i >= 0; i--) {
-    wordReverse.push(newWordSplit[i]);
+    for (let i = parolaUtente.length - 1; i >= 0; i--) {
+        parolaGirata += parolaUtente[i];      
+    }
+    return parolaGirata;
 }
 
-console.log(wordReverse);
+//Richiamo la funzione
+const parolaGirata = giraParola(newWord);
+
+if (newWord == parolaGirata) {
+    alert("Palindroma");
+} else {
+    alert("Non palindroma");
+}
+
 
 
