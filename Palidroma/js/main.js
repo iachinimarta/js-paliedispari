160c91d7f4784009b959c0d5c66607a928383063
Marta Iachini <iachinimarta@gmail.com>
2022-07-13 10:13:24 +0200

    Aggiunti commenti per capire

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -11,17 +11,25 @@ function giraParola(parolaUtente) {
 
 
     //Soluzione didattica
+
+    //Creo una variabile vuota in cui poi si inserirà la parola finale
     let parolaGirata = "";
 
+    //Scorro la parola dell utente in senso contrario
     for (let i = parolaUtente.length - 1; i >= 0; i--) {
+        
+        //A questo punto in "parolaGirata" si stamperanno le lettere della parola dell utente al contrario
         parolaGirata += parolaUtente[i];      
     }
+
+    //Il risultato sarà la variabile "parolagirata"
     return parolaGirata;
 }
 
-//Richiamo la funzione
+//Richiamo la funzione e la collego alla variabile della parola inserita nel prompt
 const parolaGirata = giraParola(newWord);
 
+//Se la parola dal prompt è uguale alla parola girata dalla funzione..
 if (newWord == parolaGirata) {
     alert("Palindroma");
 } else {
